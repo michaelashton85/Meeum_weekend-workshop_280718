6aa6cc04b7c3d990415a2774470befd52ade6c07
michaelashton85 <41742951+michaelashton85@users.noreply.github.com>
2018-07-29 13:19:18 +1000

    Navigation and Java Script

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,2 +1,17 @@
 // call css Varariables function - this helps fix the non support of custom variable in Internet Explorer 11
 cssVars();
+
+
+// open and close the NAV
+
+$('.nav-prompt').click(function(){
+
+    if($('.nav-inner').attr('aria-expanded') == 'false'){
+
+        $('.nav-inner').attr('aria-expanded', true);
+
+    } else {
+        $('.nav-inner').attr('aria-expanded', false); 
+    }
+
+});
